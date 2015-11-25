0633135753d9c957923ab23d09a54ba90b50800f
David Négrier <d.negrier@thecodingmachine.com>
2015-11-25 12:12:55 +0100

    Adding missing constructor

--- src/MethodCall.php
+++ src/MethodCall.php
@@ -17,6 +17,16 @@ class MethodCall implements MethodCallInterface
      */
     private $arguments = [];
 
+    /**
+     * @param string $methodName
+     * @param  array $arguments Array of scalar or ReferenceInterface
+     */
+    public function __construct($methodName, array $arguments)
+    {
+        $this->methodName = $methodName;
+        $this->arguments = $arguments;
+    }
+
     public function getMethodName()
     {
         return $this->methodName;
