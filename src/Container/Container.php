8b212df0c90abccddc9ed7edb8a5f209c0df257c
Guillaume Rossignol <guillaume.rossignol@wizacha.com>
2015-11-27 00:08:25 +0100

    Complete container Phpdoc

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -71,6 +71,7 @@ class Container implements ContainerInterface
      * Resolve a definition and return the resulting value.
      *
      * @param DefinitionInterface $definition
+     * @param string $requestedId
      * @return mixed
      * @throws UnsupportedDefinition
      * @throws EntryNotFound A dependency was not found.
